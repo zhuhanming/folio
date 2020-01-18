18902505869c59856471f5011d0f6f586d0dccaa
Hanming Zhu <hanming@huawen.com>
2020-01-19 01:43:14 +0800

    feat: add youtube url parsing function

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,7 +26,11 @@ const retryPromise = (promise, retriesLeft = 5, interval = 1000) => {
   });
 };
 
-export {
-  // getCurrentPathWithoutParam,
-  retryPromise
+const getYoutubeId = url => {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return match && match[2].length === 11 ? match[2] : null;
 };
+
+export { getYoutubeId, retryPromise };
