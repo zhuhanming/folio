1fd9c8d594d235ef417a20cf90e98e21c1c4bed4
Hanming Zhu <hanming@huawen.com>
2020-01-18 16:57:58 +0800

    feat: do up index.js to include contexts, router and store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from 'app';
+import AppProviders from 'contexts';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+import store, { persistor } from './app/store';
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <AppProviders>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AppProviders>
+      </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
