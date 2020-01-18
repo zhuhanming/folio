92b74afcdb6be5e42bfbbc654afa21e2704bbc9f
Hanming Zhu <hanming@huawen.com>
2020-01-18 17:01:22 +0800

    feat build dragHandle

---
+++ src/components/dragHandle/DragHandle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './DragHandle.scss';
+
+const DragHandle = props => {
+  return <i {...props} className="fas fa-bars draghandle" />;
+};
+
+export default DragHandle;
