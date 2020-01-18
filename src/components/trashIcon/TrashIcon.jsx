b1770de69b46181bde7e46f2bca96b9a79f18388
Hanming Zhu <hanming@huawen.com>
2020-01-18 17:00:45 +0800

    feat: build trashbutton

---
+++ src/components/trashIcon/TrashIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './TrashIcon.scss';
+
+const TrashIcon = props => {
+  return <i className="fas fa-trash delete-button" {...props} />;
+};
+
+export default TrashIcon;
