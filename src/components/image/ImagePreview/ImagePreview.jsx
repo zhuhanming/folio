24e6098fd7f9c6b14b42852db94cae8d07dcbe11
Hanming Zhu <hanming@huawen.com>
2020-01-19 00:37:03 +0800

    feat: add modal to image

--- src/components/image/ImagePreview/ImagePreview.jsx
+++ src/components/image/ImagePreview/ImagePreview.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+
+import Modal from 'components/modal';
 
 import './ImagePreview.scss';
 
 const ImagePreview = ({ index, image, component, size }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <Draggable draggableId={`${component.id}_${index}_${image}`} index={index}>
       {provided => (
-        <div
-          className={`image-preview__flex ${
-            // eslint-disable-next-line no-nested-ternary
-            size === 1 ? 'hundred' : size === 2 ? 'fifty' : 'thirty'
-          }`}
-          {...provided.dragHandleProps}
-        >
-          <img
-            className="image image-preview"
-            src={image}
-            alt={component.id}
-            {...provided.draggableProps}
-            ref={provided.innerRef}
-          />
-        </div>
+        <>
+          <Modal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)}>
+            <img
+              src={image}
+              className="image image-modal"
+              alt={`full-${component.id}`}
+            />
+          </Modal>
+          <div
+            className={`image-preview__flex overlay ${
+              // eslint-disable-next-line no-nested-ternary
+              size === 1 ? 'hundred' : size === 2 ? 'fifty' : 'thirty'
+            }`}
+            {...provided.dragHandleProps}
+          >
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+            <img
+              className="image image-preview"
+              src={image}
+              alt={component.id}
+              {...provided.draggableProps}
+              ref={provided.innerRef}
+              onClick={() => setIsModalOpen(true)}
+            />
+          </div>
+        </>
       )}
     </Draggable>
   );
