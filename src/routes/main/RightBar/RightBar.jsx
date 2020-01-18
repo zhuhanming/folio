831a00154449a1bf73570e42e5fd3cec3296ab2e
Hanming Zhu <hanming@huawen.com>
2020-01-18 19:45:22 +0800

    feat: fix up right sidebar

--- src/routes/main/RightBar/RightBar.jsx
+++ src/routes/main/RightBar/RightBar.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
 
 import { useSidebar } from 'contexts/SidebarContext';
+import SidebarComponent from 'components/sidebarComponent';
 
 import './RightBar.scss';
 
 const RightBar = () => {
   const { isSidebarShown } = useSidebar();
+  const rightBarContent = ['image'];
 
   return (
-    <div className={`right-bar ${isSidebarShown ? '' : 'hidden'}`}>
-      This is the rightbar.
-    </div>
+    <Droppable droppableId="right-sidebar" type="portfolio">
+      {(provided, snapshot) => (
+        <div
+          className={`right-bar ${isSidebarShown ? '' : 'hidden'}`}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          isdraggingover={snapshot.isDraggingOver.toString()}
+        >
+          {rightBarContent.map((type, index) => (
+            <SidebarComponent
+              type={type}
+              index={index}
+              key={`component-${type}`}
+            />
+          ))}
+        </div>
+      )}
+    </Droppable>
   );
 };
 export default RightBar;
