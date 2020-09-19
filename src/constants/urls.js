6c430a9d4eb8788733b652ee0baf77290aadf171
Hanming Zhu <hanming@huawen.com>
2020-09-19 13:31:39 +0800

    fix: remove cloud name from folio

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,2 @@
 export const SITE_URL = `${window.location.protocol}//${window.location.host}`;
-export const CLOUDINARY_UPLOAD_URL =
-  'https://api.cloudinary.com/v1_1/folio-hnr/image/upload';
+export const CLOUDINARY_UPLOAD_URL = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`;
